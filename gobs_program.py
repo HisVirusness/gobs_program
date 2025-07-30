dfcebe47df9ee1f6d376052da78a06b199077293
HisVirusness <75883701+HisVirusness@users.noreply.github.com>
2025-07-30 00:01:56 -0500

    Add files via upload

---
+++ gobs_program.py
@@ -0,0 +1,32 @@
+#########################################
+# The Most Important Script EVER        #
+# By Paul J Mason                       #
+#                                       #
+# I was insanely bored, and I had this  #
+# kicking around in the backburner. So, #
+# I decided to finish it.               #
+#########################################
+
+import binascii
+
+# Don't look over here, Michael...
+gob = "476f6227732050726f6772616d3a20592f4e3f"
+michael = "50656e7573"
+buster = "49277665206d61646520612068756765206d697374616b652e"
+
+# ♫ It ain't easy being white... ♫
+while True:
+    try:
+        # That's not my trick, Michael...
+        steve_holt = binascii.unhexlify(gob).decode('utf-8')
+        a = input(steve_holt + "\n? ")
+        
+        # It's my ILLUSION!
+        fake_block = binascii.unhexlify(michael).decode('utf-8')
+        while True:
+            print(fake_block, end=' ')
+    except:
+        # ♫ IT'S THE FINAL COUNTDOWN! ♫
+        buster = binascii.unhexlify(look_ma_no_hands).decode('utf-8')
+        print(look_ma_no_hands)
+ 
